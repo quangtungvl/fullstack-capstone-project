f57292a8070aa1bafdf37b49aef4e82943841efa
Quang Tung <quangtungvl@gmail.com>
2024-05-27 04:36:01 -0400

    edit registion

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -30,11 +30,15 @@ const giftRoutes = require('./routes/giftRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
 
+const authRoutes = require('./routes/authRoutes');
+
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
+
+
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
@@ -42,6 +46,7 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
